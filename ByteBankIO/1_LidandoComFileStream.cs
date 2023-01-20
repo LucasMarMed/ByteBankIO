bba89bc9b20376f7b09dcbde0624f482d0bd8a45
LucasMarMed <lucas2marques@hotmail.com>
2023-01-20 16:17:20 +0000

    Aula2
    Modificador partial que nos permite a divisão de uma definição de classe;
    Método Close de um stream, usado para fechar o fluxo e liberar determinados recursos que estavam sendo utilizados;
    StreamReader para ler textos de um arquivo;
    Os métodos Read, ReadLine e ReadToEnd, que nos ajudam a melhorar a leitura de arquivos;
    A propriedade EndOfStream colabora para verificar se a posição do fluxo está no fim dele.

---
+++ ByteBankIO/1_LidandoComFileStream.cs
@@ -0,0 +1,47 @@
+﻿using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void LidandoComFileStream()
+    {
+
+        var enderecoDoArquivo = "contas.txt";
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+            var buffer = new byte[1024]; //1KB
+
+            // public override int Read(byte[] array, int offset, int count)
+            // Devoluções:
+            // O número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+            // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+            // zero, se o final do fluxo for atingido
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+
+            fluxoDoArquivo.Close();
+            Console.ReadLine();
+
+        }
+
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+
+        /*foreach (var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
+        */
+    }
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,43 +1,13 @@
 ﻿using ByteBankIO;
 using System.Text;
 
-
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-        var buffer = new byte[1024]; //1KB
-
-        // public override int Read(byte[] array, int offset, int count)
-        // Devoluções:
-        // O número total de bytes lidos do buffer. Isso poderá ser menor que o número de
-        // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
-        // zero, se o final do fluxo for atingido
-
-        while (numeroDeBytesLidos != 0)
-        {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-            EscreverBuffer(buffer);
-        }
-                       
+               
         Console.ReadLine();
-
+                
     }
-
-    static void EscreverBuffer(byte[] buffer)
-    {
-        var utf8 = new UTF8Encoding(); 
-        var texto = utf8.GetString(buffer);
-        Console.Write(texto);
-
-        /*foreach (var meuByte in buffer)
-        {
-            Console.Write(meuByte);
-            Console.Write(" ");
-        }
-        */
-    }
+        
 }
